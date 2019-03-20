73c195f4dfdec59f8bc07b4a9c152716a94c03b1
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-03-20 11:18:58 +0900

    Added 'loading now' & refactoring

--- src/App.js
+++ src/App.js
@@ -3,34 +3,44 @@ import SearchBox from "./SearchBox";
 import CardList from "./CardList";
 import Scroll from "./Scroll";
 import "./App.css";
-import { avatars } from "./avatars.js";
+// import { avatars } from "./avatars.js";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      avatars: avatars,
+      avatars: [],
       searchfield: ""
     };
   }
 
   onSearchChange = event => {
-    // console.log(event.target.value);
     this.setState({ searchfield: event.target.value });
   };
 
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ avatars: users }));
+  }
+
   render() {
+    const { avatars, searchfield } = this.state;
+
     const filterdAvatars = avatars.filter(avatar => {
-      return avatar.name
-        .toLowerCase()
-        .includes(this.state.searchfield.toLowerCase());
+      return avatar.name.toLowerCase().includes(searchfield.toLowerCase());
     });
 
-    return (
+    return !avatars.length ? (
+      <div className="tc">
+        <h1 className="f1">Opps!</h1>
+        <p className="f2">Loading Now...</p>
+      </div>
+    ) : (
       <div className="tc">
         <h1 className="f1">Adorable Avatars</h1>
         <SearchBox searchChange={this.onSearchChange} />
-        <Scroll avatars={this.state.avatars}>
+        <Scroll>
           <CardList avatars={filterdAvatars} />
         </Scroll>
       </div>
