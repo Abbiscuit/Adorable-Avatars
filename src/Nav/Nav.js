6e90366babd61ecb36450b942c1d06ced6ece0c5
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-05-23 22:32:45 +0900

    Add about page and nav page

---
+++ src/Nav/Nav.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Nav.css";
+
+const Nav = () => {
+  const linkStyle = {
+    textDecoration: "none",
+    color: "#333"
+  };
+  return (
+    <nav className="nav">
+      <Link style={linkStyle} to="/">
+        <h1 className="logo">Adorable Avatar</h1>
+      </Link>
+      <ul className="nav-item-list">
+        <Link style={linkStyle} to="/">
+          <li className="nav-item">Home</li>
+        </Link>
+        <Link style={linkStyle} to="/about">
+          <li className="nav-item">About</li>
+        </Link>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
