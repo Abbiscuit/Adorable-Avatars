3c634c3fb0c1bb4462bdbacdd18c56f030db0346
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-07-20 13:59:01 +0900

    Add Link to card component

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './card.styles.scss';
 
 const Card = ({ name, email, id }) => {
   return (
-    <div className="card">
-      <img
-        src={`https://api.adorable.io/avatars/150/${email}`}
-        alt="avatars"
-        className="card-image"
-      />
-      <div className="card-profile">
-        <h2 className="card-name">{name}</h2>
-        <p className="card-email">{email}</p>
+    <Link to={`/${id}`}>
+      <div className="card">
+        <img
+          src={`https://api.adorable.io/avatars/150/${email}`}
+          alt="avatars"
+          className="card-image"
+        />
+        <div className="card-profile">
+          <h2 className="card-name">{name}</h2>
+          <p className="card-email">{email}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
--- src/pages/not-found.component.jsx
+++ src/pages/not-found.component.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const NotFound = props => {
   return (
-    <div className="tc grow">
+    <div style={{ textAlign: 'center' }}>
       <h1>
         No match for <i>{props.location.pathname}</i>
       </h1>
