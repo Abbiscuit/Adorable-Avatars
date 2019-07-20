bc49b9d53fcc51c0e8314aea356d7d53edcf8b99
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-07-21 08:45:08 +0900

    Updates

---
+++ precache-manifest.70f92d65cbaca583f79eb15b457460e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "24aba3a562577f6ea093",
+    "url": "/Adorable-Avatars/static/js/runtime~main.5be6183b.js"
+  },
+  {
+    "revision": "17da50d6af71c1aeac56",
+    "url": "/Adorable-Avatars/static/js/main.df0652f1.chunk.js"
+  },
+  {
+    "revision": "16d125f2cb599e7c886b",
+    "url": "/Adorable-Avatars/static/js/2.0fd41dee.chunk.js"
+  },
+  {
+    "revision": "17da50d6af71c1aeac56",
+    "url": "/Adorable-Avatars/static/css/main.024a5765.chunk.css"
+  },
+  {
+    "revision": "69e933aa0649167480bff72eb14c9e82",
+    "url": "/Adorable-Avatars/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Adorable-Avatars/precache-manifest.d6eceabc31758c82342490741ec547d7.js"
+  "/Adorable-Avatars/precache-manifest.70f92d65cbaca583f79eb15b457460e8.js"
 );
 
 workbox.clientsClaim();
