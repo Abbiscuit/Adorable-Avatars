83ec3668f459f7e5955dea7ac62066bf7daedf45
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-07-21 08:40:32 +0900

    containersフォルダを追加

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Nav from './components/nav/nav.component';
-import NotFound from './pages/not-found.component';
-import AvatarDetails from './components/avatar-details/avatar-details.component';
-import About from './pages/about/about.component';
-import HomePage from './pages/homepage/homepage.component';
+import Nav from '../components/nav/nav.component';
+import NotFound from '../pages/not-found.component';
+import AvatarDetails from '../components/avatar-details/avatar-details.component';
+import About from '../pages/about/about.component';
+import HomePage from '../pages/homepage/homepage.component';
 
 function App() {
   return (
